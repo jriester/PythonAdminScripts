50e14c53f8be9c3754cc30ed9961ef716a13a50f
James Riester <67034998+jriester@users.noreply.github.com>
2025-11-17 10:52:23 -0500

    Refactor epoch_to_datetime.py for argument parsing
    Refactored the script to include argument parsing and improved epoch timestamp replacement functionality.

--- epoch_to_datetime.py
+++ epoch_to_datetime.py
@@ -1,15 +1,32 @@
+import argparse
 from datetime import datetime
 
-file = '<path to file here>'
-file_contents = []
-with open(file, "r") as f:
-    for line in f:
-        if "|" in line:
-            date_time = datetime.utcfromtimestamp(float(line.split('|')[1])).isoformat(timespec='milliseconds')
-            file_contents.append(line.replace(line.split('|')[1], str(date_time)))
-        else:
-            file_contents.append(line)
 
-with open(file + ".new.log", "w") as file:
-    for line in file_contents:
-        file.write(line)
+def parse_args():
+    parser = argparse.ArgumentParser(description="CSV Parser")
+    parser.add_argument('-f', dest="file_path", required=True,
+                        help="/path/to/file")
+    return parser.parse_args()
+
+
+def epoch_replacement(file):
+    file_contents = []
+    print(f"Replacing epoch timestamps in {file}")
+    with open(file, "r") as f:
+        for line in f:
+            if "|" in line and "%" in line:
+                split = line.split('|')[1]
+                date_time = datetime.utcfromtimestamp(float(split)).isoformat(timespec='milliseconds')
+                file_contents.append(line.replace(split, str(date_time)))
+            else:
+                file_contents.append(line)
+        
+    with open(file + ".new.log", "w") as file:
+        for line in file_contents:
+            file.write(line)
+    print(f"Finished replacing epoch timestamps in {file.name}")
+
+
+if __name__ == '__main__':
+    file_path = parse_args().file_path
+    epoch_replacement(file_path)
